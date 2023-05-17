8917c72d3e5f2458fef74fd941c10da692421416
amit-s19 <coutamit.s19@gmail.com>
2023-05-17 17:00:40 +0530

    Added RabbitMQ and changed ports env

--- apps/gatekeeper/src/main.ts
+++ apps/gatekeeper/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.GATEKEEPER_PORT || 3000);
 }
 bootstrap();
